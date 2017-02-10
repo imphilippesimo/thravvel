06d947e5ef7d904fc200df8e50523a20cb1414c5
imphilippesimo <philippechampion58@gmail.com>
2017-02-10 08:07:59 +0100

    Annotation based Dao Test

--- src/test/java/com/thravvel/core/test/DaoTest.java
+++ src/test/java/com/thravvel/core/test/DaoTest.java
@@ -7,22 +7,18 @@ import static org.junit.Assert.assertNotNull;
 
 import java.util.ArrayList;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityTransaction;
+import javax.annotation.Resource;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.test.annotation.Commit;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
-import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.douwe.generic.dao.DataAccessException;
-import com.douwe.generic.dao.impl.GenericDao;
-import com.thravvel.core.DaoImplement.AgencyDaoImpl;
 import com.thravvel.core.DaoInterface.IAgencyDao;
 import com.thravvel.core.entities.Agency;
 import com.thravvel.core.entities.Station;
@@ -32,36 +28,15 @@ import com.thravvel.core.persistence.PersistenceConfig;
  * @author Philippe SIMO <philippechampion58@gmail.com>
  *
  */
+@Transactional
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = { PersistenceConfig.class }, loader = AnnotationConfigContextLoader.class)
+@Commit
 public class DaoTest {
 
-	// @Autowired
-	// @Resource(name = "agencyDao")
-	// TODO: REMOVE THIS STATIC DEPENDENCY INJECTION
-	IAgencyDao agencyDao = new AgencyDaoImpl();
-
-	EntityTransaction tx;
 	@Autowired
-	LocalContainerEntityManagerFactoryBean entityManagerFactory;
-	@Autowired
-	PlatformTransactionManager transactionManager;
-
-	@SuppressWarnings("rawtypes")
-	@Before
-	public void setUpBeforeClass() {
-
-		EntityManager em = entityManagerFactory.getObject().createEntityManager();
-		try {
-			((GenericDao) agencyDao).setManager(em);
-			tx = em.getTransaction();
-
-		} catch (Exception e) {
-			System.err.println("Erreur mise en place de l'environement de test de la DAO avec AgencyDao");
-			e.printStackTrace();
-		}
-
-	}
+	@Resource(name = "agencyDao")
+	IAgencyDao agencyDao;
 
 	@Test
 	public void agencyDaoShouldNotBeNull() {
@@ -71,11 +46,11 @@ public class DaoTest {
 
 	@Test
 	public void createAgencyWithoutError() {
-		Agency agency = new Agency("Touristique", new ArrayList<Station>());
+		Agency agency = new Agency("Danay", new ArrayList<Station>());
 		try {
-			tx.begin();
+
 			agencyDao.create(agency);
-			tx.commit();
+
 		} catch (DataAccessException e) {
 			System.err.println("Erreur creation nouvelle agence");
 			e.printStackTrace();
