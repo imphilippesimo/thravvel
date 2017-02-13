b4c4cdf530f816fd1d6c75c45f40ec0c79ebcc0e
imphilippesimo <philippechampion58@gmail.com>
2017-02-13 20:17:11 +0100

    Adjusted considering the use of spring data

--- src/test/java/com/thravvel/core/test/DaoTest.java
+++ src/test/java/com/thravvel/core/test/DaoTest.java
@@ -7,19 +7,14 @@ import static org.junit.Assert.assertNotNull;
 
 import java.util.ArrayList;
 
-import javax.annotation.Resource;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.annotation.Commit;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
-import org.springframework.transaction.annotation.Transactional;
 
-import com.douwe.generic.dao.DataAccessException;
-import com.thravvel.core.DaoInterface.IAgencyDao;
+import com.thravvel.core.dao.contract.IAgencyDao;
 import com.thravvel.core.entities.Agency;
 import com.thravvel.core.entities.Station;
 import com.thravvel.core.persistence.PersistenceConfig;
@@ -28,14 +23,13 @@ import com.thravvel.core.persistence.PersistenceConfig;
  * @author Philippe SIMO <philippechampion58@gmail.com>
  *
  */
-@Transactional
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = { PersistenceConfig.class }, loader = AnnotationConfigContextLoader.class)
-@Commit
+
 public class DaoTest {
 
 	@Autowired
-	@Resource(name = "agencyDao")
 	IAgencyDao agencyDao;
 
 	@Test
@@ -47,16 +41,8 @@ public class DaoTest {
 	@Test
 	public void createAgencyWithoutError() {
 		Agency agency = new Agency("Danay", new ArrayList<Station>());
-		try {
-
-			agencyDao.create(agency);
-			assertNotNull(agencyDao.findById(agency.getId()));
-
-		} catch (DataAccessException e) {
-			System.err.println("Erreur creation nouvelle agence");
-			e.printStackTrace();
-
-		}
+		agencyDao.save(agency);
+		assertNotNull(agencyDao.findOne(agency.getId()));
 
 	}
 }
